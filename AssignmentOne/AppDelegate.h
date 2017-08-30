142a8709169920a68312f50f0d91426184b56605
Gabo <gleyvamerino@smu.edu>
2017-08-30 11:06:28 -0500

    Initial Commit

---
+++ AssignmentOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AssignmentOne
+//
+//  Created by Gabriel I Leyva Merino on 8/30/17.
+//  Copyright © 2017 Leyva_Phadate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AssignmentOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AssignmentOne
+//
+//  Created by Gabriel I Leyva Merino on 8/30/17.
+//  Copyright © 2017 Leyva_Phadate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
